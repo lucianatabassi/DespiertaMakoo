a680608557048a94cccaccf73f65d94c32fc206c
lucianatabassi <lutabassi@gmail.com>
2022-10-31 16:14:16 -0300

    Ultimo 31/10

---
+++ Assets/Scripts/Scripts Escena 2/BalaEnemigo4Uno.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalaEnemigo4Uno : MonoBehaviour
+{
+     public float hit = 1;
+    public float speed = 3.0f; //variable velocidad bala
+    public Transform PuntoDisparo;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.right * Time.deltaTime * speed;
+    }
+
+
+    private void OnCollisionEnter2D (Collision2D collision) {
+        var player = collision.collider.GetComponent<MakoNivel2>();
+                //Invoke("Destruir_",2);
+       if (player) {
+            player.TakeHit (hit);
+            Destroy(gameObject);
+        }
+    }
+}
